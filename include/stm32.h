bf1cdd9ed160e506f8d1bb2d349b15daf7c14377
edlbcamargo <43915520+edlbcamargo@users.noreply.github.com>
2020-03-19 12:36:54 -0300

    Adicionando core_id da placa similar.

--- include/stm32.h
+++ include/stm32.h
@@ -10,6 +10,7 @@
 // cortex core ids
 #define STM32VL_CORE_ID 0x1ba01477
 #define STM32F7_CORE_ID 0x5ba02477
+#define CS32VL_CORE_ID 0x2ba01477  /* ADICIONEI ESSA LINHA */
 
 // Constant STM32 memory map figures
 #define STM32_FLASH_BASE           ((uint32_t)0x08000000)
